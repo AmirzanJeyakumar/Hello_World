dfaaa45047d5a8a52603ed5844c213229f0292ff
AmirzanJ <98361836+AmirzanJ@users.noreply.github.com>
2022-05-16 15:59:22 +0100

    Test working code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,11 +7,11 @@ public class Calculator {
 
          /*
            Create a calculator app for a painter:
-         * Size of the wall
-         * What happens if there is things on the wall like a door or socket
-         * How many paint buckets will I need to paint my wall
-         * What happens if I have different bucket sizes
-         * Incorporate arrays: multiple walls with different doors, sockets etc.
+           Size of the wall
+           What happens if there is things on the wall like a door or socket
+           How many paint buckets will I need to paint my wall
+           What happens if I have different bucket sizes
+           Incorporate arrays: multiple walls with different doors, sockets etc.
          */
 
         System.out.println("Calculator Initialised!");
@@ -51,20 +51,15 @@ public class Calculator {
         Iterate through the array we created and at every index (wall), we will prompt the user to provide the number of
         objects that we do not want to paint per wall. In a For loop, create two arrays that represent the height and
         width of these objects for every wall that the user has specified.
-         */
+        */
 
         ArrayList<Double> objectAreaList = new ArrayList<>();
 
-        for (int wallIndex=0; wallIndex < wallAreaList.size(); wallIndex++) {
-            System.out.println("Are there areas of wall (number: " + (wallIndex+1) +") that you do not want to paint? E.g doors, windows and sockets");
-            System.out.println("Type in \"Yes\" or \"No\"");
-            String excludeAnswer = scanner.nextLine();
+        for (int wallIndex=0; wallIndex < numOfWalls; wallIndex++) {
 
-            if (excludeAnswer.equalsIgnoreCase("Yes")) {
-
-                System.out.println("How many different objects do you want to exclude from painting on this wall?");
-                int numOfObjects = scanner.nextInt();
-
+            System.out.println("How many different objects do you want to exclude from painting on wall number: " + (wallIndex+1) +"?");
+            int numOfObjects = scanner.nextInt();
+            if (numOfObjects>0) {
                 for (int object = 0; object < numOfObjects; object++) {
 
                     System.out.println("What is the height of this object?");
@@ -74,11 +69,38 @@ public class Calculator {
                     double objectWidth = scanner.nextDouble();
 
                     double objectArea = objectHeight * objectWidth;
-                    objectAreaList.add(object,objectArea);
+                    objectAreaList.add(object, objectArea);
 
                 }
             }
         }
+
+        /*
+        We now have two arrays which contain area values for every wall and the total area of objects
+        Created a final array with the area taking into account the excluded area
+        */
+
+        ArrayList<Double> finalAreaList = new ArrayList<>();
+        double totalFinalArea = 0;
+
+        for (int wall=0; wall < numOfWalls; wall++ ) {
+            double finalArea = wallAreaList.get(wall) - objectAreaList.get(wall);
+            finalAreaList.add(finalArea);
+        }
+
+        for (int wall=0; wall < numOfWalls; wall++ ) {
+            totalFinalArea += finalAreaList.get(wall);
+        }
+
+        System.out.println("Area that needs to be painted: " + totalFinalArea);
+        System.out.println("What is the volume of your paint bucket?");
+        double bucketVolume = scanner.nextDouble();
+
+        double paintVolume = totalFinalArea * 0.1;
+        System.out.println("Volume of paint required: " + paintVolume);
+
+        double bucket = Math.ceil(paintVolume/bucketVolume);
+        System.out.println("Number of buckets required: " + bucket);
     }
 
 
